b43a95b6e4164bec799ff8b5947ac4205c0763f8
Lawrence Mok <lawrence.mok@amlogic.com>
2011-11-17 18:10:20 -0800

    rename audio.primary.default to audio.primary.amlogic
    set as optional

--- Android.mk
+++ Android.mk
@@ -22,8 +22,9 @@ ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 	ALSAMixer.cpp \
 	ALSAControl.cpp
 
-  LOCAL_MODULE := audio.primary.default
+  LOCAL_MODULE := audio.primary.amlogic
   LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+  LOCAL_MODULE_TAGS := optional
 
   LOCAL_STATIC_LIBRARIES += libmedia_helper
   LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
